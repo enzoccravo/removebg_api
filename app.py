2736ce1212a764c8d8862dfc98897ab4264a0406
enzoCravo <122815351+enzoccravo@users.noreply.github.com>
2026-07-21 19:15:56 -0300

    Refactor remove_bg function and update server status

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
-
 os.environ["U2NET_HOME"] = "/tmp/.u2net"
 
 from fastapi import FastAPI, Request, Response
-from rembg import remove, new_session
 import uvicorn
 import base64
-from PIL import Image
 import io
 import gc
 
@@ -19,19 +16,17 @@ app = FastAPI()
 
 my_session = None
 
-def get_session():
-    global my_session
-    if my_session is None:
-        print("loading ai. first time")
-        my_session = new_session("u2netp")
-    return my_session
-
 @app.get("/")
 def root():
-    return {"status": "api on"}
+    return {"status": "server on"}
 
 @app.post("/remove-bg")
 async def remove_bg(request: Request):
+    from rembg import remove, new_session
+    from PIL import Image
+    
+    global my_session
+    
     content = await request.body()
     
     img = Image.open(io.BytesIO(content))
@@ -44,10 +39,12 @@ async def remove_bg(request: Request):
     del content
     gc.collect()
 
-    session = get_session()
+    if my_session is None:
+        print("Carregando o modelo de IA u2netp...")
+        my_session = new_session("u2netp")
+        
+    output_image = remove(small_content, session=my_session)
     
-    output_image = remove(small_content, session=session)
-
     encoded_image = base64.b64encode(output_image).decode('utf-8')
     return encoded_image
 
